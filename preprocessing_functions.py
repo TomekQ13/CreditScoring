f78c45d4456a63d1ade2caf66cc176e0f7345e44
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2020-03-21 13:49:16 +0100

    functions separation

---
+++ preprocessing_functions.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+############## PREPORCESSING FUNCTIONS ###########################
+
+def unique_count(dataframe):
+    #shows how many unique values are in the columns of a dataframe
+    uniques = dataframe.from_records([(col, dataframe[col].nunique()) for col in dataframe.columns], 
+                                        columns=['Column_Name', 'Num_Unique']).sort_values(by=['Num_Unique'])
+    uniques.reset_index(inplace=True)
+    return uniques
+
+def DeleteMissing(dataframe, perc_missing_delete = 0.6):
+    return_df = dataframe[dataframe.columns[dataframe.isnull().mean() <= perc_missing_delete]]
+    return return_df
+
+def get_column_names_from_ColumnTransformer(column_transformer):    
+    col_name = []
+    for transformer_in_columns in column_transformer.transformers_[:-1]:#the last transformer is ColumnTransformer's 'remainder'
+        raw_col_name = transformer_in_columns[1]
+        if isinstance(transformer_in_columns[1],Pipeline): 
+            transformer = transformer_in_columns[1].steps[-1][1]
+        else:
+            transformer = transformer_in_columns[1]
+        try:
+            names = transformer.get_feature_names()
+        except AttributeError: # if no 'get_feature_names' function, use raw column name
+            names = raw_col_name
+        if isinstance(names,np.ndarray): # eg.
+            col_name += names.tolist()
+        elif isinstance(names,list):
+            col_name += names    
+        elif isinstance(names,str):
+            col_name.append(names)
+    return col_name
+
+def Imputer_df(dataframe, num_cols_names, cat_cols_names, strategy_num_cols = 'mean',
+               strategy_cat_cols = 'most_frequent', remainder = 'passthrough'):
+    
+    #imputes missing values using SimpleImputer from sklearn and returns a dataframe instead of numpy array
+    from sklearn.impute import SimpleImputer
+    
+    names = num_cols+cat_cols+[name for name in dataframe.columns if name not in num_cols+cat_cols]
+    ct1 = ColumnTransformer([
+        ('imput_mean', SimpleImputer(strategy = strategy_num_cols), num_cols),
+        ('imput_most_freq', SimpleImputer(strategy = strategy_cat_cols), cat_cols)
+        ], remainder = 'passthrough')
+    return_df = pd.DataFrame(data = ct1.fit_transform(dataframe), columns = names)
+    return return_df
+
+
+def ColumnValuesCheck(df1, df2):
+    # Columns in both dataframes must have the same names
+    #checks if columns with the same names have the same values. Columns do not have to be in the same order
+    #DO NOT USE AFTER IMPUTING MISSING VALUES
+    list = []
+    for column in df1:
+        column_values1 = df1[column]
+        column_values2 = df2[column]
+        if column_values1.equals(column_values2):
+            list = list + [1]
+        else:
+            list = list + [0]
+    return list
+
+
+def MakeDummies(data, cat_cols):
+    for cat in enumerate(cat_cols):
+        #add category names to the values - this way get_dummies will create columns with names
+        data[cat[1]] = data[cat[1]].apply(lambda x : str(cat[1]) +  '_' + str(x))
+        
+        #get dummies
+        dummy = pd.get_dummies(data[cat[1]])
+        
+        #select all column except the last one
+        dummy = dummy.iloc[:,0:dummy.shape[1]-1].copy()
+        
+        data = data.merge(dummy, left_index = True, right_index = True)
+        #alternatively data = pd.concat([data, dummy], axis = 1)
+        
+    #remove original columns from the dataset
+    data.drop(cat_cols, axis = 1, inplace = True)
+    return data
+
+def train_test_split_df(data_X, y, test_size = 0.2, random_state = 0):
+    X_train, X_test, y_train, y_test = train_test_split(data_X, y, test_size = 0.2, random_state = 0)
+    y_train = pd.DataFrame(data = y_train, columns = ['default3'])
+    y_test = pd.DataFrame(data = y_test, columns = ['default3'])
+    return X_train, X_test, y_train, y_test
+
---
+++ problem_functions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+############## PROBLEM SPECIFIC FUNCTIONS ###########################
+def MaxDueInstallmentsMinPeriod(data_transactions):
+    #Creating table with product id, first installment period adn max due installements.
+    max_due_install_min_period = pd.pivot_table(data = data_transactions, index = 'aid',
+                                            values = ['due_installments', 'period'], aggfunc = ['max', 'min'])
+    max_due_install_min_period.columns = ['max_due_installments', 'max_period', 'min_due_installments', 'min_period']
+    max_due_install_min_period.drop(['max_period', 'min_due_installments'], axis = 1, inplace = True)
+    max_due_install_min_period['min_period'] = pd.to_numeric(max_due_install_min_period['min_period'])
+    return max_due_install_min_period
+
+def DefaultPerProduct(data_product, n_installments_due = 3, max_period = 200712):
+    #Creating dataframe with 0-1 variable if the product defaulted
+    col_name = 'default'+str(n_installments_due)
+    data_product.insert(len(data_product.columns),col_name, 0)
+    for index, row in data_product.iterrows():        
+        if row['max_due_installments'] >= n_installments_due and row['min_period'] <= max_period:
+            data_product.at[index, col_name] = 1
+        else:
+            data_product.at[index, col_name] = 0
+    data_product.drop(['min_period', 'max_due_installments'], axis = 1, inplace = True)
+    return data_product
+
+def ProductionDefault(data_product_default, data_production):
+    #Adding col1umn with 0-1 default to the main dataframe
+    data_production['period'] = pd.to_numeric(data_production['period'])
+    data_production = data_production[data_production['period'] <= 200712]
+    data_merged = data_production.merge(right = data_product_default, on = 'aid')
+    return data_merged
+
