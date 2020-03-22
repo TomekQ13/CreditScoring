056b9452cfd784a831e29f808918278c439da6e0
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2020-03-22 18:22:44 +0100

    gradient boosting

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -5,8 +5,11 @@
 
 
 ############## PREPORCESSING FUNCTIONS ###########################
+import pandas as pd
+import numpy as np
 
 def unique_count(dataframe):
+    import pandas as pd
     #shows how many unique values are in the columns of a dataframe
     uniques = dataframe.from_records([(col, dataframe[col].nunique()) for col in dataframe.columns], 
                                         columns=['Column_Name', 'Num_Unique']).sort_values(by=['Num_Unique'])
@@ -14,10 +17,12 @@ def unique_count(dataframe):
     return uniques
 
 def DeleteMissing(dataframe, perc_missing_delete = 0.6):
+    import pandas as pd
     return_df = dataframe[dataframe.columns[dataframe.isnull().mean() <= perc_missing_delete]]
     return return_df
 
-def get_column_names_from_ColumnTransformer(column_transformer):    
+def get_column_names_from_ColumnTransformer(column_transformer):   
+    import pandas as pd
     col_name = []
     for transformer_in_columns in column_transformer.transformers_[:-1]:#the last transformer is ColumnTransformer's 'remainder'
         raw_col_name = transformer_in_columns[1]
@@ -38,15 +43,18 @@ def get_column_names_from_ColumnTransformer(column_transformer):
     return col_name
 
 def Imputer_df(dataframe, num_cols_names, cat_cols_names, strategy_num_cols = 'mean',
-               strategy_cat_cols = 'most_frequent', remainder = 'passthrough'):
-    
+               strategy_cat_cols = 'most_frequent', remainder = 'drop'):    
+    import pandas as pd
+    import numpy as np
     #imputes missing values using SimpleImputer from sklearn and returns a dataframe instead of numpy array
-    from sklearn.impute import SimpleImputer
     
-    names = num_cols+cat_cols+[name for name in dataframe.columns if name not in num_cols+cat_cols]
+    from sklearn.impute import SimpleImputer
+    from sklearn.compose import ColumnTransformer
+
+    names = num_cols_names+cat_cols_names+[name for name in dataframe.columns if name not in num_cols_names+cat_cols_names]
     ct1 = ColumnTransformer([
-        ('imput_mean', SimpleImputer(strategy = strategy_num_cols), num_cols),
-        ('imput_most_freq', SimpleImputer(strategy = strategy_cat_cols), cat_cols)
+        ('imput_num', SimpleImputer(strategy = strategy_num_cols), num_cols_names),
+        ('imput_cat', SimpleImputer(strategy = strategy_cat_cols), cat_cols_names)
         ], remainder = 'passthrough')
     return_df = pd.DataFrame(data = ct1.fit_transform(dataframe), columns = names)
     return return_df
@@ -56,6 +64,7 @@ def ColumnValuesCheck(df1, df2):
     # Columns in both dataframes must have the same names
     #checks if columns with the same names have the same values. Columns do not have to be in the same order
     #DO NOT USE AFTER IMPUTING MISSING VALUES
+    import pandas as pd
     list = []
     for column in df1:
         column_values1 = df1[column]
@@ -67,7 +76,10 @@ def ColumnValuesCheck(df1, df2):
     return list
 
 
-def MakeDummies(data, cat_cols):
+def MakeDummiesAndScale(data, cat_cols, num_cols):
+    import pandas as pd
+    from sklearn.preprocessing import StandardScaler
+    
     for cat in enumerate(cat_cols):
         #add category names to the values - this way get_dummies will create columns with names
         data[cat[1]] = data[cat[1]].apply(lambda x : str(cat[1]) +  '_' + str(x))
@@ -80,12 +92,17 @@ def MakeDummies(data, cat_cols):
         
         data = data.merge(dummy, left_index = True, right_index = True)
         #alternatively data = pd.concat([data, dummy], axis = 1)
-        
     #remove original columns from the dataset
     data.drop(cat_cols, axis = 1, inplace = True)
+    
+    sc = StandardScaler()
+    data[num_cols] = sc.fit_transform(data[num_cols])
+    
     return data
 
 def train_test_split_df(data_X, y, test_size = 0.2, random_state = 0):
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(data_X, y, test_size = 0.2, random_state = 0)
     y_train = pd.DataFrame(data = y_train, columns = ['default3'])
     y_test = pd.DataFrame(data = y_test, columns = ['default3'])
--- problem_functions.py
+++ problem_functions.py
@@ -2,20 +2,23 @@
 # coding: utf-8
 
 # In[ ]:
-
+import pandas as pd
+import pandas
 
 ############## PROBLEM SPECIFIC FUNCTIONS ###########################
 def MaxDueInstallmentsMinPeriod(data_transactions):
+    import pandas
     #Creating table with product id, first installment period adn max due installements.
-    max_due_install_min_period = pd.pivot_table(data = data_transactions, index = 'aid',
+    max_due_install_min_period = pandas.pivot_table(data = data_transactions, index = 'aid',
                                             values = ['due_installments', 'period'], aggfunc = ['max', 'min'])
     max_due_install_min_period.columns = ['max_due_installments', 'max_period', 'min_due_installments', 'min_period']
     max_due_install_min_period.drop(['max_period', 'min_due_installments'], axis = 1, inplace = True)
-    max_due_install_min_period['min_period'] = pd.to_numeric(max_due_install_min_period['min_period'])
+    max_due_install_min_period['min_period'] = pandas.to_numeric(max_due_install_min_period['min_period'])
     return max_due_install_min_period
 
 def DefaultPerProduct(data_product, n_installments_due = 3, max_period = 200712):
     #Creating dataframe with 0-1 variable if the product defaulted
+    import pandas
     col_name = 'default'+str(n_installments_due)
     data_product.insert(len(data_product.columns),col_name, 0)
     for index, row in data_product.iterrows():        
@@ -27,6 +30,7 @@ def DefaultPerProduct(data_product, n_installments_due = 3, max_period = 200712)
     return data_product
 
 def ProductionDefault(data_product_default, data_production):
+    import pandas
     #Adding col1umn with 0-1 default to the main dataframe
     data_production['period'] = pd.to_numeric(data_production['period'])
     data_production = data_production[data_production['period'] <= 200712]
